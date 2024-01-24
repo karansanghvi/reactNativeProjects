4dc149a6d633e20f8b24accd5152b938901c6f47
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-01-24 16:02:13 +0530

    authenticationSystem

--- App.js
+++ App.js
@@ -1,15 +1,91 @@
-import { View, Text } from 'react-native'
-import React, { useState, useEffect } from 'react'
-import { NavigationContainer } from "@react-navigation/native"
-import { createStackNavigator } from "@react-navigation/stack"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { firebase } from './config';
+import Login from './src/Login';
+import Registration from './src/Registration';
+import Dashboard from './src/Dashboard';
+import Header from './src/components/Header';
 
-const App = () => {
+const Stack = createStackNavigator();
+
+function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null); 
+
+  function onAuthStateChanged(user) {
+    setUser(user); 
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return () => subscriber(); 
+  }, []);
+
+  if (initializing) return null;
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <Stack.Navigator>
+      {!user ? (
+        <>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerTitle: () => <Header name="Bug Ningas" />,
+              headerStyle: {
+                height: 150,
+                borderBottomLeftRadius: 50,
+                borderBottomRightRadius: 50,
+                backgroundColor: '#00e4d0',
+                shadowColor: '#000',
+                elevation: 25
+              }
+            }}
+          />
+
+          <Stack.Screen
+            name="Registration"
+            component={Registration}
+            options={{
+              headerTitle: () => <Header name="Bug Ningas" />,
+              headerStyle: {
+                height: 150,
+                borderBottomLeftRadius: 50,
+                borderBottomRightRadius: 50,
+                backgroundColor: '#00e4d0',
+                shadowColor: '#000',
+                elevation: 25
+              }
+            }}
+          />
+        </>
+      ) : (
+        <Stack.Screen
+          name="Dashboard"
+          component={Dashboard}
+          options={{
+            headerTitle: () => <Header name="Dashboard" />,
+            headerStyle: {
+              height: 150,
+              borderBottomLeftRadius: 50,
+              borderBottomRightRadius: 50,
+              backgroundColor: '#00e4d0',
+              shadowColor: '#000',
+              elevation: 25
+            }
+          }}
+        />
+      )}
+    </Stack.Navigator>
+  );
 }
 
-export default App
+export default () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+}
